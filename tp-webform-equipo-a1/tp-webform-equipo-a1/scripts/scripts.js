9229de865933388e6d08bb5d98ec58b3acff73ae
Mauro Asselborn <mauasselborn@gmail.com>
2024-05-13 21:57:46 -0300

    Se agrega funcionalidad de sumar cantidades

--- tp-webform-equipo-a1/tp-webform-equipo-a1/scripts/scripts.js
+++ tp-webform-equipo-a1/tp-webform-equipo-a1/scripts/scripts.js
@@ -1,1 +1,16 @@
-﻿
+﻿let btnSuamar = document.querySelector("#btnSumar");
+let btnRestar = document.querySelector("#btnRestar");
+let detalleCantidad = document.querySelector("#detalleCantidad");
+
+btnSuamar.addEventListener('click', function (e) {
+    if (detalleCantidad.value < 50) {
+        detalleCantidad.value++;
+    }
+})
+
+
+btnRestar.addEventListener('click', function (e) {
+    if (detalleCantidad.value > 0) {
+        detalleCantidad.value--;
+    }
+})
